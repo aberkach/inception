8e514ff1f5e667d7368dfe0d40948152cf54be90
Abdelfattah Berkach <abberkac@e3r8p15.1337.ma>
2024-09-07 23:50:39 +0100

    add something in wordpress

--- Makefile
+++ Makefile
@@ -1,17 +1,50 @@
+WP_DATA = ./srcs/requirements/wordpress/data #define the path to the wordpress data
+DB_DATA = ./srcs/requirements/mariadb/data #define the path to the mariadb data
 
+# default target
 all: up
 
-up:
-	docker-compose -f srcs/docker-compose.yml up --build
+# start the biulding process
+# create the wordpress and mariadb data directories.
+# start the containers in the background and leaves them running
+up: build
+	@mkdir -p $(WP_DATA)
+	@mkdir -p $(DB_DATA)
+	docker-compose -f ./srcs/docker-compose.yml up 
 
+# stop the containers
 down:
-	docker-compose -f srcs/docker-compose.yml down
+	docker-compose -f ./srcs/docker-compose.yml down
 
+# stop the containers
+stop:
+	docker-compose -f ./srcs/docker-compose.yml stop
+
+# start the containers
+start:
+	docker-compose -f ./srcs/docker-compose.yml start
+
+# build the containers
+build:
+	docker-compose -f ./srcs/docker-compose.yml build
+
+# clean the containers
+# stop all running containers and remove them.
+# remove all images, volumes and networks.
+# remove the wordpress and mariadb data directories.
+# the (|| true) is used to ignore the error if there are no containers running to prevent the make command from stopping.
 clean:
-	docker-compose -f srcs/docker-compose.yml down -v
+	@docker stop $$(docker ps -qa) || true
+	@docker rm $$(docker ps -qa) || true
+	@docker rmi -f $$(docker images -qa) || true
+	@docker volume rm $$(docker volume ls -q) || true
+	@docker network rm $$(docker network ls -q) || true
+	@rm -rf $(WP_DATA) || true
+	@rm -rf $(DB_DATA) || true
 
-fclean: clean
-	docker system prune -af
+# clean and start the containers
+re: clean up
 
-re: fclean all
-.PHONY: all up down clean fclean re
+# prune the containers: execute the clean target and remove all containers, images, volumes and networks from the system.
+prune: clean
+	@docker system prune -a --volumes -f
--- srcs/requirements/wordpress/conf/wp-init.sh
+++ srcs/requirements/wordpress/conf/wp-init.sh
@@ -8,54 +8,64 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
 # go to wordpress directory
-cd /var/www/html/wordpress
+cd /var/www/wordpress
 # give permission to wordpress directory
-chmod -R 755 /var/www/html/wordpress
+chmod -R 755 /var/www/wordpress/
 # change owner of wordpress directory to www-data
-chown -R www-data:www-data /var/www/html/wordpress
-
+chown -R www-data:www-data /var/www/wordpress
 #---------------------------------------------------ping mariadb---------------------------------------------------#
 # check if mariadb container is up and running
 ping_mariadb_container() {
-    nc -zv mariadb 3306 > /dev/null
-    return $? # return exit status of the ping
+    nc -zv mariadb 3306 > /dev/null # ping the mariadb container
+    return $? # return the exit status of the ping command
 }
 
-start_time=$(date +%s)
-end_time=$((start_time + 30)) # set to 30 seconds
+start_time=$(date +%s) # get the current time in seconds
+end_time=$((start_time + 30)) # set the end time to 30 seconds after the start time
 
-while [ $(date +%s) -lt $end_time ]; do
-    ping_mariadb_container
-    if [ $? -eq 0 ]; then
+while [ $(date +%s) -lt $end_time ]; do # loop until the current time is greater than the end time
+    ping_mariadb_container # Ping the MariaDB container
+    if [ $? -eq 0 ]; then # Check if the ping was successful
         echo "[========MARIADB IS UP AND RUNNING========]"
-        break
+        break # Exit the loop if MariaDB is up
     else
         echo "[========WAITING FOR MARIADB TO START...========]"
-        sleep 1
+        sleep 1 # Wait for 1 second before trying again
     fi
 done
 
-if [ $(date +%s) -ge $end_time ]; then
-    echo "[========MARIADB DID NOT RESPOND IN TIME========]"
-    exit 1
+if [ $(date +%s) -ge $end_time ]; then # check if the current time is greater than or equal to the end time
+    echo "[========MARIADB IS NOT RESPONDING AFTER 30 SECONDS! CHECK CONNECTION SETTINGS!========]"
 fi
-
-#---------------------------------------------------wp installation---------------------------------------------------#
+#---------------------------------------------------wp installation---------------------------------------------------##---------------------------------------------------wp installation---------------------------------------------------#
 
 # download wordpress core files
 wp core download --allow-root
+
+# Grant necessary privileges to the database user
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
+USE $MYSQL_DB;
+GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
+
 # create wp-config.php file with database details
-wp core config --dbhost=mariadb:3306 --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
-# install wordpress with site details
-wp core install --url="$DOMAIN_NAME" --title="INCEPTION" --admin_user="$WP_ADMIN_N" --admin_password="$WP_ADMIN_P" --admin_email="$WP_ADMIN_E" --allow-root
-# create a new WordPress user
-wp user create "$WP_U_NAME" "$WP_U_EMAIL" --role=editor --user_pass="$WP_U_PASS" --allow-root
+wp admin create --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --path=/var/www/html --allow-root
+
+# Install WordPress
+wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_N" --admin_password="$WP_ADMIN_P" --admin_email="$WP_ADMIN_E" --allow-root
+
+# Create a new user
+wp user create "$WP_U_NAME" "$WP_U_EMAIL" --user_pass="$WP_U_PASS" --role="$WP_U_ROLE" --allow-root
 
 #---------------------------------------------------php config---------------------------------------------------#
 
-# change listen port in php-fpm config from unix socket to port 9000
-sed -i 's@listen = /run/php/php7.4-fpm.sock@listen = 9000@' /etc/php/7.4/fpm/pool.d/www.conf
-# create the necessary directory for php-fpm
+# change listen port from unix socket to 9000
+sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
+# create a directory for php-fpm
 mkdir -p /run/php
-# start php-fpm in the foreground
+# start php-fpm service in the foreground to keep the container running
 /usr/sbin/php-fpm7.4 -F
+
+# Restart Nginx to pick up changes
+nginx -s reload
