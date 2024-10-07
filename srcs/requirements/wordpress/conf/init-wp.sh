ed59cf636ea1f3d22eb1254c77857800eb1c670c
Abdelfattah Berkach <abberkac@e1r3p5.1337.ma>
2024-10-07 20:15:01 +0100

    fix user already exist err

--- srcs/requirements/wordpress/conf/init-wp.sh
+++ srcs/requirements/wordpress/conf/init-wp.sh
@@ -3,12 +3,13 @@
 # Check database connectivity before proceeding
 until mysql -h $DB_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" > /dev/null 2>&1; do
     echo "Waiting for MariaDB to be ready..."
-    sleep 2
+    sleep 5
 done
 
 # Update PHP-FPM config
 sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 mkdir -p /run/php
+
 
 # Create WordPress directory and set permissions 
 mkdir -p /var/www/html
@@ -18,6 +19,8 @@ cd /var/www/html
 
 if [ ! -e wp-config.php ]; then
     wp core download --allow-root
+    # install sendmail
+    apt-get update -y && apt-get install -y sendmail
     # Create wp-config.php and check if it succeeds
     wp config create --allow-root --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST:3306
 fi
@@ -34,12 +37,9 @@ fi
 
 # create a new user for WordPress if it does not exist
 if ! wp user list --allow-root --role=editor --field=$WP_U_NAME | grep -q $WP_U_NAME; then
-    wp user create --allow-root \
-       $WP_U_NAME $WP_U_EMAIL \
-       --user_pass=$WP_U_PASS \
-       --role=$WP_U_ROLE \
-       --user-email=$WP_U_EMAIL
+    wp user create --allow-root $WP_U_NAME $WP_U_EMAIL --user_pass=$WP_U_PASS --role=$WP_U_ROLE 
 fi
+
 
 
 # Start PHP-FPM
