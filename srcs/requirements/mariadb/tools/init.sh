6f01713c7e7f7dce32e388b6f55861a70d2ff854
Abdelfattah Berkach <abberkac@e1r3p5.1337.ma>
2024-10-07 22:52:54 +0100

    fix

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -7,13 +7,13 @@ service mariadb start
 sleep 5
 
 # Create database and user for WordPress to use (if not exist) and grant all privileges
-mariadb -u root -e "
+mariadb -u root  -e "
 CREATE DATABASE IF NOT EXISTS $MYSQL_DB;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO '$MYSQL_USER'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 "
+
 
 # # grant all privileges to the newly created user
 # mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
